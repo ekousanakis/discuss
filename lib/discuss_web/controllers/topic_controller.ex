42703cead6d0943cd031ab2a8d625f146d52f579
ekousanakis <ekousanakis>
2018-08-22 21:38:22 +0300

    create new topic and redirect

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -3,8 +3,25 @@ defmodule DiscussWeb.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Discuss.Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    render conn, "new.html"
+    render conn, "new.html", changeset: changeset
   end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Discuss.Repo.insert(changeset) do
+      {:ok, post} -> conn
+                    |> put_flash(:info, "Topic Created")
+                    |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} -> render conn, "new.html", changeset: changeset
+    end
+  end
+
 end
--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -16,8 +16,9 @@ defmodule DiscussWeb.Router do
   scope "/", DiscussWeb do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", TopicController, :index
     get "/topics/new", TopicController, :new
+    post "/topics", TopicController, :create
   end
 
   # Other scopes may use custom stacks.
